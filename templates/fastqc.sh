2529c858baf6cc050ec7476c49d0fa557fcb7c14
Logan Wallace <loganw1011@gmail.com>
2025-01-16 14:29:05 -0800

    Update fastqc.sh
    adding module

--- templates/fastqc.sh
+++ templates/fastqc.sh
@@ -7,6 +7,9 @@ set -euo pipefail
 # Run the cluster command
 echo "Running fastqc"
 
+# Module Load
+ml FastQC/0.12.1-Java-11
+
 mkdir fastqc_${Sample}_logs
 fastqc -o fastqc_${Sample}_logs -t 6 -f fastq -q $R1 $R2
 #rm $R1 $R2 to avoid repetitive upload to the S3 bucket
