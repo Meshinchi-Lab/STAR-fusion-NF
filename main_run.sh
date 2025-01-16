39ead3bf31a523a531f2b78c7d29a4f8e2225bfc
Logan Wallace <loganw1011@gmail.com>
2025-01-16 11:29:26 -0800

    Update main_run.sh

--- main_run.sh
+++ main_run.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
-set -e
-DATE=$(date +%F)
-NFX_CONFIG=./nextflow.singularity.config
+#SBATCH --job-name=STAR-Fusion-NF
+#SBATCH --output=./slurmout/STAR-Fusion-NF-%j.out
 
-# Load the modules
-ml nextflow/20.09.0-edge
-ml Singularity
-export PATH=$SINGULARITYROOT/bin/:$PATH
+# Module Loading
+ml Nextflow/23.04.2
 
-# Declare the output directories 
-FH_BASE_BUCKET="s3://fh-pi-meshinchi-s-eco-public"
-STAR_FUSION_OUT=$FH_BASE_BUCKET/TARGET_AML/RNAseq_Illumina_Data/STAR_Fusion/
-MULTIQC_OUT=$FH_BASE_BUCKET/TARGET_AML/RNAseq_Illumina_Data/multiQC/
-STAR_INDEX_OUT=$FH_BASE_BUCKET/Reference_Data/CICERO_Ref/GRCh37_gencode_v19_STAR_idx/
-STAR_ALIGNER_OUT=$FH_BASE_BUCKET/TARGET_AML/RNAseq_Illumina_Data/CICERO/
-CICERO_OUT=$FH_BASE_BUCKET/TARGET_AML/RNAseq_Illumina_Data/CICERO/
-
-#More Info:
-#https://github.com/FusionInspector/FusionInspector/wiki/FusionInspector-Outputs-Described
-
-#Execute the nextflow index workflow
-#TARGET_AML_Remission_JMML_APL_MDAnderson_sample_sheet.txt
-nextflow run -c ${NFX_CONFIG} main.nf \
-    --sample_sheet sample_sheets/test_sample_sheet.txt \
-    --star_genome_lib $FH_BASE_BUCKET/Reference_Data/GRCh37_gencode_v19_CTAT_lib_Oct012019/ctat_genome_lib_build_dir/ \
-    --STAR_Fusion_out $STAR_FUSION_OUT \
-    --multiQC_out $MULTIQC_OUT \
-    --fasta_file $FH_BASE_BUCKET/Reference_Data/CICERO_Ref/Homo_sapiens/GRCh37-lite/FASTA/GRCh37-lite.fa \
-    --gtf_url "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz" \
-    --star_index_out $STAR_INDEX_OUT \
-    --STAR_aligner_out $STAR_ALIGNER_OUT \
-    --cicero_genome_lib $FH_BASE_BUCKET/Reference_Data/CICERO_Ref/ \
-    --CICERO_out $CICERO_OUT \
-    -entry star_index \
-    -with-report generate_index_${DATE}_singularity.html \
-    -cache  TRUE \
-    -resume
+# Run the nextflow workflow
+nextflow \ 
+    run \ 
+    main.nf \ 
+    
