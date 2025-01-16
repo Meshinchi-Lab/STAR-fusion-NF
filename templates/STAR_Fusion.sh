0d944703d3a63660784c4572dcc47bcf3319fb3c
Logan Wallace <loganw1011@gmail.com>
2025-01-16 11:57:18 -0800

    Update STAR_Fusion.sh

--- templates/STAR_Fusion.sh
+++ templates/STAR_Fusion.sh
@@ -6,7 +6,16 @@ set -euo pipefail
 echo ""
 
 # Run the STAR Fusion Command
-echo "STAR Fusion"
+echo "Running STAR Fusion"
 
+STAR-Fusion --genome_lib_dir \$PWD/$genome_lib \
+	  	--chimeric_junction "${Sample}Chimeric.out.junction" \
+		  --left_fq $R1 \
+		  --right_fq $R2 \
+	  	--CPU 8 \
+	  	--FusionInspector inspect \
+	  	--examine_coding_effect \
+	  	--denovo_reconstruct \
+	  	--output_dir $Sample
 
 echo "STAR Fusion Complete"
