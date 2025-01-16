192d5d071f6a2a0cda691b34e5b00f40a3ee660b
Logan Wallace <loganw1011@gmail.com>
2025-01-16 11:58:41 -0800

    Update STAR_Fusion.sh

--- templates/STAR_Fusion.sh
+++ templates/STAR_Fusion.sh
@@ -10,8 +10,8 @@ echo "Running STAR Fusion"
 
 STAR-Fusion --genome_lib_dir \$PWD/$genome_lib \
 	  	--chimeric_junction "${Sample}Chimeric.out.junction" \
-		  --left_fq $R1 \
-		  --right_fq $R2 \
+		--left_fq $R1 \
+		--right_fq $R2 \
 	  	--CPU 8 \
 	  	--FusionInspector inspect \
 	  	--examine_coding_effect \
