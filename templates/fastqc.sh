9a4d09d027c0bde4af5d50f4817aaa1c3de5bff0
Logan Wallace <loganw1011@gmail.com>
2025-01-16 14:51:59 -0800

    Update fastqc.sh

--- templates/fastqc.sh
+++ templates/fastqc.sh
@@ -12,7 +12,8 @@ ml FastQC/0.12.1-Java-11
 mkdir fastqc_${Sample}_logs
 
 # Run FASTQC on each of R1 and R2 of Sample
-fastqc -o fastqc_${Sample}_logs \
+fastqc \ 
+    -o fastqc_${Sample}_logs \
     -t 6 \
     -f fastq \
     -q $R1 $R2
